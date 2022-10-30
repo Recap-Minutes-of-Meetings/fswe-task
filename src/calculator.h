3ff4697529b9e531c541e90d1db1881974355b24
Mikhail Kuznetsov <46815928+MikhailKuz@users.noreply.github.com>
2022-10-31 00:08:06 +0300

    Update calculator.h

--- src/calculator.h
+++ src/calculator.h
@@ -21,14 +21,13 @@ struct List
 class Calculator
 {
 public:
-  int calc(const char *s, struct List *prev);
+  int calc(const char *s, struct List *prev = nullptr);
 
 private:
   int prior(int c);
   int count(int a, int op, int b);
   int is_aryphmetic(char a);
   void rec_delete(struct List *l);
-  int calc(const char *s, struct List *prev = nullptr);
   int last_calc(struct List *prev, int cur_num);
 };
 
