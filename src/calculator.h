a1403ef43d65b3a9883334ea8ac7dbf4e86b30ec
Maximelian Mumladze <Makcieml@gmail.com>
2022-10-31 04:10:22 +0300

    upd calculator h

--- src/calculator.h
+++ src/calculator.h
@@ -1,12 +1,21 @@
 #ifndef __CALCULATOR_H__
 #define __CALCULATOR_H__
 
+
 #include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ctime>   
 
 struct Exception
 {
   const char *message;
-  Exception(const char *message) : message(message) {}
+  Exception(const char *message) : message(message) {
+    std::ofstream out("ErrorLog.txt", std::ios::out | std::ios::app);
+    std::time_t m = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    out << std::ctime(&m) << "\t\t" << message <<'\n';
+    out.close();
+  }
 };
 
 struct List
@@ -28,6 +37,7 @@ private:
   int count(int a, int op, int b);
   int is_aryphmetic(char a);
   void rec_delete(struct List *l);
+  //int calc(const char *s, struct List *prev = nullptr);
   int last_calc(struct List *prev, int cur_num);
 };
 
